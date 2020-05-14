1e13014389a10302293acd320657238c7f3c0b6f
Chris Kirkham <chris.kirkham11@gmail.com>
2020-05-14 03:57:19 +0100

    Made basic test script for moving particles via wind field wind. It worksgit add .

---
+++ Assets/Scripts/MoveParticlesInWindField.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class MoveParticlesInWindField : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+    public WindField windField;
+    public bool active = true;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        if (windField == null) Debug.LogError("No wind field given for WindFieldProducer " + ToString() + "!");
+    }
+
+    private void Update()
+    {
+        if(active)
+        {
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+            particleSystem.GetParticles(particles);
+            for (int i = 0; i < particles.Length; i++)
+            {
+                Vector3 pos = particles[i].position;
+                particles[i].position = Vector3.MoveTowards(pos, pos + windField.GetWind(pos), 1f);
+            }
+
+            particleSystem.SetParticles(particles);
+        }
+        
+    }
+}
